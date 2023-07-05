cce0ffeec7cb503162bebbf1408cd3c4cfb3ea52
divyesh1707 <divyeshramoliya707@gmail.com>
2023-07-05 09:10:51 +0530

    05-07 Delete Data

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Doctors from '../admin/Container/Doctors';
 import Department from '../admin/Container/Department';
-import Medicines from '../admin/Container/Medicines'
+import Medicines from '../admin/Container/Medicines/Medicines'
 import Appointment from '../user/Container/Appointment';
 import Layout from '../admin/Component/Layout';
 import CustomTable from '../admin/Container/CustomTable';
---
+++ src/admin/Container/Medicines/Medicines.js
@@ -0,0 +1,128 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import MedicineForm from './MedicineForm';
+
+
+export default function FormDialog() {
+
+  const [items, setItems] = React.useState([]);
+  const [update, setUpdate] = React.useState(null);
+
+  const handleSubmitData = (data) => {
+    console.log(data);
+
+    let rno = Math.floor(Math.random() * 1000);
+
+    let newData = { id: rno, ...data };
+
+    let localdata = JSON.parse(localStorage.getItem("medicines"));
+
+    console.log(localdata);
+
+    if (localdata === null) {
+      localStorage.setItem("medicines", JSON.stringify([newData]))
+      setItems([newData])
+    } else {
+
+      if (update) {
+        let uData = localdata.map((v) => {
+          if (v.id === data.id) {
+            return data
+          } else {
+            return v;
+          }
+        })
+        localStorage.setItem("medicines", JSON.stringify(uData))
+        setItems(uData)
+        console.log(uData);
+      } else {
+        localdata.push(newData)
+        localStorage.setItem("medicines", JSON.stringify(localdata))
+        setItems(localdata)
+      }
+
+     
+    }
+
+    
+    setUpdate(null);
+    
+  };
+
+
+  const handleDelete = (id) => {
+    let localData = JSON.parse(localStorage.getItem("medicines"));
+
+    let fdata = localData.filter((v, i) => v.id !== id)
+
+    localStorage.setItem("medicines", JSON.stringify(fdata))
+
+    setItems(fdata)
+  }
+
+  React.useEffect(() => {
+    let localData = JSON.parse(localStorage.getItem("medicines"));
+
+    if (localData !== null) {
+      setItems(localData)
+    }
+
+  }, []);
+
+  // const handleEdit = (data) => {
+
+  //   handleClickOpen();
+
+  //   formik.setValues(data);
+
+  //   console.log(data);
+
+  //   setUpdate(data);
+
+  // }
+
+  const columns = [
+
+    // { field: 'id', headerName: 'ID', width: 130 },
+    { field: 'name', headerName: 'Name', width: 130 },
+    { field: 'date', headerName: 'ExpiryDate', width: 130 },
+    { field: 'price', headerName: 'Price', width: 130 },
+    { field: 'desc', headerName: 'Description', width: 130 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 130,
+      renderCell: (params) => (
+        <>
+          <IconButton aria-label="delete" onClick={() => handleDelete(params.row.id)}>
+            <DeleteIcon />
+          </IconButton>
+
+          {/* <IconButton aria-label="edit" onClick={() => handleEdit(params.row)}>
+            <EditIcon />
+          </IconButton> */}
+        </>
+      ),
+
+    }
+
+  ];
+
+  return (
+    <>
+      <MedicineForm Adddata={handleSubmitData}/>
+
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={items}
+          columns={columns}
+          pageSizeOptions={[5, 10]}
+          checkboxSelection
+        />
+      </div>
+    </>
+  );
+}
